789003e6507f099473fa7fc262d09e3067aad2d8
noprysk <noprysk@n-ix.com>
2016-04-19 12:55:27 +0300

    Spring Scheduling Tasks - base

---
+++ src/main/java/nao/cycledev/springtest/SpringtestApplication.java
@@ -0,0 +1,14 @@
+package nao.cycledev.springtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+//@EnableScheduling
+public class SpringtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtestApplication.class, args);
+	}
+}
